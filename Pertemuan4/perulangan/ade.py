9520369d2d20561a8948cf5f4fb5e7cda4572f3a
adepasihatangkeallo <adepasiha@gmail.com>
2024-09-25 16:31:08 +0800

    pertemuan 4 FIX

---
+++ Pertemuan4/perulangan/ade.py
@@ -0,0 +1,41 @@
+# ulang = 10
+# for i in range(ulang):
+# #     print("Perulangan ke-" + str(i))
+
+
+# for i in range(2,10,):
+#     print(f"halo {i}")
+
+
+# for i in range(2,10,2):
+#     print(f"halo {i}")
+
+
+for i in range(10,2,-2):
+    print(f"halo {i}")
+
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#     hitung += 1
+#     jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range(250):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
