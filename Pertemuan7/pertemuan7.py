cc216912fdd43c808bf4b859fe1cacac3ff07cc7
adepasihatangkeallo <adepasiha@gmail.com>
2024-10-16 16:06:37 +0800

    Add files via upload

---
+++ Pertemuan7/pertemuan7.py
@@ -0,0 +1,54 @@
+# def mhs():
+#     print("Hello")
+
+# mhs()
+
+# def kali():
+#     x = 6*4
+#     print(x)
+
+# Membuat fungsi dengan parameter (panjang, lebar)
+# def luas_persegi_panjang(panjang, lebar):
+#     luas = panjang * lebar
+#     print ("Luas persegi panjang:", luas ,"hasil")
+#     print (f"Luas persegi panjang: (luas cm)")
+
+# luas_persegi_panjang(4, 6)
+
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     jumlah = sisi + sisi
+#     return luas, jumlah
+
+
+# hasil = luas_persegi (4) + luas_persegi(2)
+# print(hasil)
+
+# def mhs(nama, nim, nilai = 40):
+#     print(nama)
+#     print(nim)
+#     print(nilai)
+
+
+# mhs("ucup", "20", "20", data = 2, data2 = 3)
+
+
+# data = 10
+
+# def nilai():
+#     data =20
+#     print(data)
+
+# print()
+# print(data)
+
+# def data(kumpulan):
+#     print(kumpulan)
+
+# value  = [1,2,3,4]
+# data(value)
+
+def mhs(nama1, nama2, nama3):
+    print(nama3)
+
+mhs(nama3 = "ucup",)
